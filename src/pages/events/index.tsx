259cb4f8cb3027ebfe9316ab36d2b8906131f566
Wioleta Tyranska <wioleta.tyranska@gmail.com>
2023-05-13 17:52:45 +0200

    Add temporary songkick api events mockup

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,19 +1,39 @@
+import { SongkickEventObject } from "@/types/events/events";
+import { useEffect, useState } from "react";
+
 export const Events = () => {
+  const [events, setEvents] = useState<undefined | Array<SongkickEventObject>>(
+    undefined
+  );
+
+  useEffect(() => {
+    const getEventsList = async () => {
+      const result = await fetch("http://localhost:3000/api/events");
+      const response = await result.json();
+      setEvents(response);
+    };
+    getEventsList();
+  }, []);
+
   return (
     <section>
       <h1>events</h1>
       <table>
         <thead>
           <tr>
+            <td>name</td>
             <td>date</td>
             <td>venue</td>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>date</td>
-            <td>venue</td>
-          </tr>
+          {events?.map(({ displayName, start, venue }) => (
+            <tr key={displayName}>
+              <td>{displayName}</td>
+              <td>{start.date}</td>
+              <td>{venue.displayName}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </section>
